4e24755ce1754328997440ad73c7c2bad82bf31d
mrrukkus <doooog1@rambler.ru>
2019-09-05 14:42:48 +0500

    ё

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,19 @@
 var modal = document.querySelector(".modal");
 var weekProduct = document.querySelector(".product-card__order");
 var cartProduct = document.querySelectorAll(".item__cart")
-var cartProductArray = Array.prototype.slice.call(cartProduct);
 
-weekProduct.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modal.classList.add("modal-show");
-})
+if (document.querySelector(".product-card__order")) {
+  weekProduct.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.add("modal-show");
+  })
+}
 
 if (document.querySelector(".modal")) {
-  var modalOverlay = document.querySelector(".modal"),
-    modalButtons = document.querySelectorAll(".item__cart");
-  modalButtonArray = Array.prototype.slice.call(modalButtons);
-
-  modalButtonArray.forEach(function (el) {
-    el.addEventListener("click", function (e) {
-      e.preventDefault();
-      modalOverlay.classList.add("modal-show");
-    });
-  });
+  for (var i = 0; i < cartProduct.length; i++) {
+    cartProduct[i].addEventListener("click", function(evt) {
+      evt.preventDefault();
+      modal.classList.add("modal-show");
+    })
+  }
 }
